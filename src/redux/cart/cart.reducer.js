4ecc4a8048c5a3f6ef615cbe77b54de2bcb6a708
Samira Ouraga <saouraga@microsoft.com>
2020-05-10 13:14:32 -0700

    updated shopdata with new data, updated cart utils to group items in cart

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,7 +1,10 @@
 import CartActionTypes from './cart.types';
 
+import {addItemToCart } from './cart.utils';
+
 const INITIAL_STATE ={
-    hidden:true
+    hidden:true,
+    cartItems:[]
 };
 
 
@@ -11,12 +14,18 @@ const cartReducer = (state = INITIAL_STATE, action)=>{
             return {
                 ...state,
                 hidden: !state.hidden
-            }
-            break;
+            };
+
+        case CartActionTypes.ADD_ITEM:
+            return{
+                ...state,
+                cartItems:addItemToCart(state.cartItems, action.payload)
+            };
+
     
         default:
             return state;
-            break;
+
     }
 }
 
